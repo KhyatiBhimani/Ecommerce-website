82926a3651470e3f194708f144e80a3db004cfa3
KhyatiBhimani <khyatibhimani047@gmail.com>
2024-04-15 10:30:48 +0530

    Ecommerce-website-in ReactJs

---
+++ frontend/src/components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Description.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">
+            Description
+        </div>
+        <div className="descriptionbox-nav-box fade">
+           Reviews(122)
+        </div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum commodi, quas doloribus expedita fugit numquam nulla quos fuga. Molestiae dicta aliquam alias voluptas perferendis cumque at officia unde vel modi obcaecati non voluptatem fugit animi, quae culpa cupiditate aliquid quaerat doloremque ea doloribus, ipsum officiis!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus et libero fuga inventore. Perspiciatis reiciendis consectetur aut similique molestias quae, porro sunt, pariatur autem laboriosam qui, quaerat nam doloribus atque!</p>
+      
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
